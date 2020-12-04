aa9a53dbf481eb4f51839fcaa0e980b61d8bf1c7
PedisettiVenkatesh <75016701+PedisettiVenkatesh@users.noreply.github.com>
2020-12-04 12:41:29 +0530

    Create source_(Library_in_C).c

---
+++ source_(Library_in_C).c
@@ -0,0 +1,9 @@
+#include<stdio.h>   //including Standard input output header file
+#include<stdlib.h>  //including Standard library header file
+#include<string.h>  //including String header file
+#include<ctype.h>   //including the ctype header file
+
+void main()
+{
+  printf("Welcome to Library system");
+}
