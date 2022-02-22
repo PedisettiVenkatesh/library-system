11c727cf88c713c4c73b2500084c9f6aaec13a89
PedisettiVenkatesh <75016701+PedisettiVenkatesh@users.noreply.github.com>
2022-02-23 00:29:58 +0530

    init book

--- py/library.py
+++ py/library.py
@@ -7,3 +7,25 @@ import sys
 
 # %%
 pritn('Library In Python')
+
+
+class Book():
+    def __init__(self,title,author,bookid):
+        self.title = title
+        self.author = author
+        self.bookid = bookid
+
+    def borrow():
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
