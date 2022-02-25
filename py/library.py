33cb37ec949f17957e48ce60ea4ee7e86cca4aee
PedisettiVenkatesh <75016701+PedisettiVenkatesh@users.noreply.github.com>
2022-02-25 18:38:24 +0530

    welcome screen function

--- py/library.py
+++ py/library.py
@@ -1,14 +1,74 @@
-# %%
 # Library System in Python
+# By- PedisettiVenkatesh
 
-# %%
-# packages
+from math import floor
+import os
+from random import choice
 import sys
+import numpy as np
 
-# %%
-# print('Library In Python')
+symbol = '·'
+col, row = os.get_terminal_size()
+if col < 100:
+    print('This Program is better viewed in terminal size greater than 100.\nPlease ReSize the terminal and run again')
+    exit()
+elif col % 2 == 1:
+    window_length = col-1
+else:
+    window_length = col
 
-# %%
+
+def cls():
+    """
+    Clear the console screen
+    """
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def get_choice():
+    choice = input(symbol+'  Your Choice : ')
+    return choice
+
+
+def line():
+    print(symbol*window_length)
+
+
+def space_line():
+    print(symbol+' '*(window_length-2)+symbol)
+
+
+def centered_text(text):
+    print(symbol+' '*int((window_length - len(text))/2-1) +
+          text+' '*int((window_length - len(text))/2-1)+symbol)
+
+
+def lefted_text(text):
+    print(symbol+text+' '*int((window_length - len(text)-2))+symbol)
+
+
+def welcome_screen():
+    line()
+    space_line()
+    centered_text('Library System In Python')
+    space_line()
+    line()
+    space_line()
+    lefted_text('  Enter the values repectively...')
+    centered_text('1  Search Book')
+    centered_text('2  Login      ')
+    centered_text('3  Register   ')
+    centered_text('4  Exit       ')
+    space_line()
+    line()
+
+    choice = get_choice()
+    cls()
+    return choice
+
+
+cls()
+print(welcome_screen())
 
 
 class Book():
@@ -19,19 +79,3 @@ class Book():
 
     def borrow():
         pass
-
-
-# %%
-book1 = Book('demoName', 'me', 'p12')
-
-# print('name', book1.name)
-
-# %%
-
-
-def welcomeScreen():
-    print('Library System In Python')
-
-
-# %%
-welcomeScreen()
